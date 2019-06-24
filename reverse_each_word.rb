4d9c4bdcc6bde5575bc7c25278422ca0bd3532de
Aida Abdikulova <abdikulova@gmail.com>
2019-06-24 18:09:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence1)
   new_array = sentence1.split
-  second_array = new_array.each do |word|
+  second_array = new_array.collect do |word|
     word.reverse!
   end
-  return second_array.join(" ")
 end
