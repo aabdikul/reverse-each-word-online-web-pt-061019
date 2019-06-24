5b8df34b6e3e978129a59a1f964ff2d6bc4b93b8
Aida Abdikulova <abdikulova@gmail.com>
2019-06-24 18:01:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence1)
   new_array = sentence1.split
-  second_array = new_array.reverse
-  second_array.each do |word|
+  new_array.each do |word|
     word.reverse!
   end
 end
